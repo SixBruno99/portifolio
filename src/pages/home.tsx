af4706cc3c2c5f4326b84191e24527225f4ff18f
Bruno Henrique <brunohc2016@gmail.com>
2023-11-16 14:45:26 -0300

    feat: added new text and image animations

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,53 @@
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Image,
+  Text,
+  keyframes,
+  usePrefersReducedMotion,
+} from "@chakra-ui/react";
 
 import Foto from "../assets/Images/foto.jpg";
 import { SocialMedias } from "../core/components/social";
 
+const imageChange = keyframes`
+  0% {
+    clip-path: polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%);
+  }
+  50% {
+    clip-path: polygon(0 0, 75% 0, 100% 100%, 25% 100%);
+  }
+  100% {
+    clip-path: polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%);
+  }
+
+`;
+
+const textInitialization = keyframes`
+  0% {
+    margin-top: 50px;
+    opacity: 0
+  }
+  50% {
+    opacity: 0.5
+  }
+  100% {
+    margin-top: 0
+    opacity: 1.0
+  }
+`;
+
 export function Home() {
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  const imageAnimation = prefersReducedMotion
+    ? undefined
+    : `${imageChange} infinite  5s ease-in`;
+
+  const textAnimation = prefersReducedMotion
+    ? undefined
+    : `${textInitialization} 1  2.5s ease-in`;
+
   return (
     <Flex
       minHeight="calc(100vh - 112px)"
@@ -16,13 +60,13 @@ export function Home() {
     >
       <Flex width="full" justifyContent="space-evenly">
         <Box>
-          <Text fontSize="22px" fontWeight="bold" color="#F5F5F5">
+          <Text fontSize="22px" fontWeight="bold" color="#F5F5F5" animation={textAnimation}>
             Bruno Henrique
           </Text>
-          <Text fontSize="32px" fontWeight="bold" color="#F5F5F5">
+          <Text fontSize="32px" fontWeight="bold" color="#F5F5F5" animation={textAnimation}>
             Front End Developer
           </Text>
-          <Text marginTop={4} color="#F5F5F5">
+          <Text marginTop={4} color="#F5F5F5" animation={textAnimation}>
             Criando Interfaces Intuitivas e Experiências Incríveis no
             <br />
             Desenvolvimento Web de Alto Nível.
@@ -32,14 +76,7 @@ export function Home() {
           </Flex>
         </Box>
         <Box>
-          <Image
-            src={Foto}
-            width="300"
-            h="400"
-            clipPath={
-              "polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%)"
-            }
-          />
+          <Image animation={imageAnimation} src={Foto} width="300" h="400" />
         </Box>
       </Flex>
       {/* <iframe src="https://brunohenrique.netlify.app/" width="400" height="400">
