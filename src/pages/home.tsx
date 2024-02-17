c783c132589f578f157a4e91b3ad4fbe48d27e01
Bruno Henrique <brunohc2016@gmail.com>
2024-02-17 01:50:27 -0300

    feat: projects section ready

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,20 @@
 import {
   Box,
   Flex,
+  Heading,
   Image,
   Text,
   keyframes,
   usePrefersReducedMotion,
 } from "@chakra-ui/react";
 
-import Foto from "../assets/Images/foto.jpg";
 import { SocialMedias } from "../core/components/social";
 import { Projects } from "../core/components/projects";
+
+// Images imports
+import Foto from "../assets/Images/foto.jpg";
+import Curriculo from "../assets/Images/curriculo.png";
+import Guia from "../assets/Images/guia-leticia.png";
 
 const imageChange = keyframes`
   0% {
@@ -53,7 +58,7 @@ export function Home() {
     <Flex
       minHeight="calc(100vh - 112px)"
       maxWidth="7xl"
-      marginTop={12}
+      marginY={8}
       marginX="auto"
       paddingInline="5"
       alignItems="center"
@@ -93,18 +98,40 @@ export function Home() {
         </Box>
       </Flex>
 
-      <Flex
-        maxWidth="full"
-        minWidth="7xl"
-        overflowX="scroll"
-        marginY={12}
-        gap={5}
-      >
-        <Projects projectLink="https://brunohenrique.netlify.app/" title="CV" />
-        <Projects projectLink="https://brunohenrique.netlify.app/" title="CV" />
-        <Projects projectLink="https://brunohenrique.netlify.app/" title="CV" />
-        <Projects projectLink="https://brunohenrique.netlify.app/" title="CV" />
-        <Projects projectLink="https://www.hltv.org/A" title="CV" />
+      <Heading marginY={12} color="white">
+        Projects
+      </Heading>
+
+      <Flex maxWidth="full" minWidth="7xl" overflowX="scroll" gap={5}>
+        <Projects
+          projectImage={Curriculo}
+          projectLink="https://brunohenrique.netlify.app/"
+          title="Currículo programado"
+          description="Oi"
+          technologies="React"
+          scrollTime="1s"
+        />
+        <Projects
+          projectImage={Guia}
+          projectLink="http://localhost:3000/"
+          title="landing page"
+          description="Oi"
+          technologies="React"
+          scrollTime="5s"
+        />
+        <Projects
+          projectImage={Curriculo}
+          projectLink="https://brunohenrique.netlify.app/"
+          title="CV"
+          scrollTime="2s"
+        />
+        <Projects
+          projectImage={Curriculo}
+          projectLink="https://brunohenrique.netlify.app/"
+          title="CV"
+          scrollTime="2s"
+        />
+        <Projects projectImage={Curriculo} scrollTime="2s" />
       </Flex>
     </Flex>
   );
